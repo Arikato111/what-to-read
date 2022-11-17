58c671a15c409ce661c2f9d347a907f9a244e4f9
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2022-11-17 12:24:22 +0700

    add new book and fixed size book

--- src/components/ImgShow.tsx
+++ src/components/ImgShow.tsx
@@ -11,7 +11,7 @@ const ImgShow: FC<ImgShowInput> = ({ url, img, name }) => {
     <div className="text-center">
       <a target="_blank" href={url}>
         <img
-          className="sm:opacity-90 hover:opacity-100 shadow-lg shadow-gray-400 dark:hover:shadow-gray-700 sm:shadow-transparent hover:shadow-gray-400 transition-all w-[35vh] inline-block border-2 p-1 bg-white dark:bg-slate-800 rounded border-purple-400  duration-300"
+          className="sm:opacity-90 hover:opacity-100 shadow-lg shadow-gray-400 dark:hover:shadow-gray-700 sm:shadow-transparent hover:shadow-gray-400 transition-all w-[35vh] 2xl:w-[30vh] inline-block border-2 p-1 bg-white dark:bg-slate-800 rounded border-purple-400  duration-300"
           src={img}
           title={name ?? ""}
           alt="book image"
