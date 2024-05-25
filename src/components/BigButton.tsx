37ccd6ac687cffc321e367bf1d1f3e94d01e14a4
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-05-25 13:36:44 +0700

    change big button style in darkmode

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -8,7 +8,7 @@ interface BT {
 const BigButton: FC<BT> = ({ onclick, title }) => {
   return (
     <button
-      className="p-2 bg-green-400 dark:bg-green-600 hover:bg-green-500/90 dark:hover:bg-green-500 shadow shadow-zinc-200 dark:shadow-zinc-500 hover:shadow-zinc-500/90 rounded text-white text-4xl active:scale-95 active:shadow-none transition-all duration-200"
+      className="p-2 bg-green-400 dark:bg-green-600 hover:bg-green-500/90 dark:hover:bg-green-500 shadow shadow-zinc-200 dark:shadow-zinc-500 hover:shadow-zinc-500/90 rounded text-white text-4xl active:scale-95 active:shadow-none transition-all duration-200 dark:shadow-none"
       onClick={() => onclick()}
       title="คลิกเพื่อสุ่ม"
     >
